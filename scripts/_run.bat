b63cf0625f8c51582f2814bf60e6a82fd72186ac
Detrav <witalyezep@gmail.com>
2024-11-09 15:36:31 +0300

    * update scripts

--- scripts/_run.bat
+++ scripts/_run.bat
@@ -3,7 +3,9 @@ pushd ".."
   FOR /F %%g IN ('git rev-list --count --all') do (SET BUILD_NUMBER=%%g)
 
   pushd "src/DetravRecipeCalculator.Desktop"
-    dotnet publish -r win-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false /p:AssemblyVersion=0.5.0.%BUILD_NUMBER% -o "%~dp0../_build/out"
+    dotnet publish -r win-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false  /p:PublishSingleFile=true /p:AssemblyVersion=0.5.0.%BUILD_NUMBER% -o "%~dp0../_build/win-x64"
+    dotnet publish -r linux-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=true /p:AssemblyVersion=0.5.0.%BUILD_NUMBER% -o "%~dp0../_build/linux-x64"
+    dotnet publish -r osx-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false  /p:PublishSingleFile=true /p:AssemblyVersion=0.5.0.%BUILD_NUMBER% -o "%~dp0../_build/osx-x64"
   popd
 
 popd

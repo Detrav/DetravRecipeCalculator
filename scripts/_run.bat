8f95219a5712e4db5514685e966fcbe8b39898e4
Detrav <witalyezep@gmail.com>
2024-11-11 23:44:45 +0300

    * update version

--- scripts/_run.bat
+++ scripts/_run.bat
@@ -3,9 +3,9 @@ pushd ".."
   FOR /F %%g IN ('git rev-list --count --all') do (SET BUILD_NUMBER=%%g)
 
   pushd "src/DetravRecipeCalculator.Desktop"
-    dotnet publish -r win-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false  /p:PublishSingleFile=true /p:AssemblyVersion=0.8.0.%BUILD_NUMBER% -o "%~dp0../_build/DetravRecipeCalculator-win-x64"
-    dotnet publish -r linux-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=true /p:AssemblyVersion=0.8.0.%BUILD_NUMBER% -o "%~dp0../_build/DetravRecipeCalculator-linux-x64"
-    dotnet publish -r osx-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false  /p:PublishSingleFile=true /p:AssemblyVersion=0.8.0.%BUILD_NUMBER% -o "%~dp0../_build/DetravRecipeCalculator-osx-x64"
+    dotnet publish -r win-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false  /p:PublishSingleFile=true /p:AssemblyVersion=0.9.0.%BUILD_NUMBER% -o "%~dp0../_build/DetravRecipeCalculator-win-x64"
+    dotnet publish -r linux-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=true /p:AssemblyVersion=0.9.0.%BUILD_NUMBER% -o "%~dp0../_build/DetravRecipeCalculator-linux-x64"
+    dotnet publish -r osx-x64 -c Release --self-contained /p:DebugType=None /p:DebugSymbols=false  /p:PublishSingleFile=true /p:AssemblyVersion=0.9.0.%BUILD_NUMBER% -o "%~dp0../_build/DetravRecipeCalculator-osx-x64"
   popd
 
 popd

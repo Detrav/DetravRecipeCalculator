e50161042161515e8c51b7542bc673e4878850d8
Detrav <witalyezep@gmail.com>
2024-11-11 20:00:07 +0300

    + added subgraph logic

--- src/DetravRecipeCalculator/ViewModels/SubGraphNodeVM.cs
+++ src/DetravRecipeCalculator/ViewModels/SubGraphNodeVM.cs
@@ -1,6 +1,7 @@
 ﻿using DetravRecipeCalculator.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,16 @@ namespace DetravRecipeCalculator.ViewModels
 
         public SubGraphNodeVM(GraphEditorVM parent) : base(parent)
         {
+            TimeToCraft = 1;
+        }
+
+
+        public override void FixPins()
+        {
+            base.FixPins();
+
+            SyncItems(Input);
+            SyncItems(Output);
         }
 
         public override NodeModel SaveState()
@@ -26,11 +37,64 @@ namespace DetravRecipeCalculator.ViewModels
         {
             graphModel = model.GraphModel;
             base.RestoreState(model);
+            TimeToCraft = 1;
         }
 
         public override void Build()
         {
             base.Build();
         }
+
+        private void SyncItems(ObservableCollection<ConnectorOutVM> pins)
+        {
+            var oldList = pins.ToList();
+            pins.Clear();
+
+            if (graphModel != null)
+            {
+
+                foreach (var item in graphModel.Inputs)
+                {
+                    var pin = oldList.FirstOrDefault(m => m.Name == item.Key);
+                    if (pin == null)
+                        pin = new ConnectorOutVM(this, item.Key);
+                    pin.Value = item.Value;
+                    pins.Add(pin);
+                }
+            }
+        }
+
+        private void SyncItems(ObservableCollection<ConnectorInVM> pins)
+        {
+            var oldList = pins.ToList();
+            pins.Clear();
+
+            if (graphModel != null)
+            {
+                foreach (var item in graphModel.Outputs)
+                {
+                    var pin = oldList.FirstOrDefault(m => m.Name == item.Key);
+                    if (pin == null)
+                        pin = new ConnectorInVM(this, item.Key);
+                    pin.Value = item.Value;
+                    pins.Add(pin);
+                }
+            }
+
+        }
+
+        public GraphEditorVM LoadSubGraph()
+        {
+            var vm = new GraphEditorVM();
+            if (graphModel != null)
+                vm.RestoreState(graphModel);
+            return vm;
+        }
+
+        public void SaveSubGraph(GraphEditorVM graph)
+        {
+            graphModel = graph.SaveState() as GraphModel;
+        }
+
     }
 }
